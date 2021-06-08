16873e8467aec2ff326f80cfb57a1e7ac9016a3d
alansaw90 <alanjiawei90@gmail.com>
2021-06-08 16:27:58 +0800

    number 2

---
+++ place_of__birth_checker.rb
@@ -0,0 +1,36 @@
+puts 'Which Malaysia state are you from?'
+ic = gets.chomp
+
+if ic == '01'
+  puts("johor")
+elsif ic == '02'
+  puts("Kedah")
+elsif ic == '03'
+  puts("Kelantan")
+elsif ic == '04'
+  puts("Melacca")
+elsif ic == '05'
+  puts("Negeri Sembilan")
+elsif ic == '06'
+  puts("Pahang")
+elsif ic == '07'
+  puts("Penang")
+elsif ic == '08'
+  puts("Perak")
+elsif ic == '09'
+  puts("Perlis")
+elsif ic == '10'
+  puts("Selangor")
+elsif ic == '11'
+  puts("Terengganu")
+elsif ic == '12'
+  puts("Sabah")
+elsif ic == '13'
+  puts("Sarawak")
+elsif ic == '14'
+  puts("Kuala Lumpur")
+elsif ic == '15'
+  puts("labuan")
+elsif ic == '16'
+  puts("Putrajaya")
+end
